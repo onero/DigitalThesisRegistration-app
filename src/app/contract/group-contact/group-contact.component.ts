f1d5225eb002f94ab43034bbd7c0ae639d20c3dd
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-11-27 10:44:31 +0100

    Able to read emails from database

--- src/app/contract/group-contact/group-contact.component.ts
+++ src/app/contract/group-contact/group-contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-group-contact',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupContactComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  isEditable: boolean;
+  @Input()
+  email: string;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
