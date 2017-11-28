ed106ec21e8c00e472d54343ad48296c7d9a4b9c
Sk0vgaard <Sk0vgaard@live.dk>
2017-11-28 19:48:10 +0100

    FormBuilder with validators on createCompany

--- src/app/contract/company/company.component.ts
+++ src/app/contract/company/company.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Company} from '../shared/company.model';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {CompanyService} from '../shared/company.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-company',
@@ -12,14 +13,35 @@ export class CompanyComponent implements OnInit {
 
   @Input()
   company: Company;
+  companyGroup: FormGroup;
 
   companies: Company[];
 
-  constructor(private modalService: NgbModal, private companyService: CompanyService) {
+  constructor(private modalService: NgbModal, private companyService: CompanyService, private fb: FormBuilder) {
     companyService.getAll().subscribe(c => this.companies = c);
+    this.setValidators();
   }
 
   ngOnInit() {
+  }
+
+  setValidators() {
+    this.companyGroup = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      contactName: ['', [Validators.required, Validators.minLength(2)]],
+      contactEmail: ['', [Validators.required, Validators.minLength(5)]],
+      contactPhone: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  isValid(controlName: string) {
+    const control = this.companyGroup.controls[controlName];
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.companyGroup.controls[controlName];
+    return !control.invalid && (control.dirty || control.touched);
   }
 
   open(content) {
@@ -29,5 +51,9 @@ export class CompanyComponent implements OnInit {
 
   selectCompany(company: Company) {
     this.company = company;
+  }
+
+  createCompany() {
+
   }
 }
