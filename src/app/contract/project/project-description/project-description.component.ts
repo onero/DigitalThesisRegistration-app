b7fa9a826bff4eb369798cf295d34f6a1dae8093
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-11-28 13:28:20 +0100

    Implemented Project: Description

--- src/app/contract/project/project-description/project-description.component.ts
+++ src/app/contract/project/project-description/project-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project-description',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDescriptionComponent implements OnInit {
 
+  @Input()
+  description: string;
   constructor() { }
 
   ngOnInit() {
