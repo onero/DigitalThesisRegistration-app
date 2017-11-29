a435e4a43898b2c31c35d819131b04c108c6a959
Sk0vgaard <Sk0vgaard@live.dk>
2017-11-29 12:09:49 +0100

    Changing mock out with real data.

--- src/app/contract/shared/contract.service.ts
+++ src/app/contract/shared/contract.service.ts
@@ -1,8 +1,15 @@
 import {Observable} from 'rxjs/Observable';
 import {Contract} from './contract.model';
-import {Student} from './student.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
 
+@Injectable()
 export class ContractService {
+
+  url = environment.RestAPI + '/contracts';
+
+  constructor(private http: HttpClient) {}
 
   mockContracts: Contract[] = [
     {title: 'Mr. Nice', groupId: 1, projectId: 1, companyId: 1},
@@ -18,6 +25,10 @@ export class ContractService {
 
   addContract(contract: Contract) {
     this.mockContracts.push(contract);
+  }
+
+  createContract(contract: Contract): Observable<Contract> {
+    return this.http.post<Contract>(this.url, contract);
   }
 
   getById(groupId: number, projectId: number, companyId: number): Contract {
