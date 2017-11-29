d0a01ed65ab14f53cc821bb83c44f14f952011d4
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-11-29 12:23:22 +0100

    Added isEditable on all project components
    Also added project components in editCOntract html

--- src/app/contract/project/project-period/project-period.component.ts
+++ src/app/contract/project/project-period/project-period.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-project-period',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectPeriodComponent implements OnInit {
 
+  @Input()
+  isEditable: boolean;
   startModel;
   endModel;
   constructor() { }
