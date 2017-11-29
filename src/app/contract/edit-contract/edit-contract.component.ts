883e95b28a18cedffc94bbc13f07f76d8a61c065
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-11-29 12:43:52 +0100

    Now showing the title of the project in editContract

--- src/app/contract/edit-contract/edit-contract.component.ts
+++ src/app/contract/edit-contract/edit-contract.component.ts
@@ -7,8 +7,10 @@ import {forEach} from '@angular/router/src/utils/collection';
 import {StudentService} from '../shared/student.service';
 import {Group} from '../shared/group.model';
 import {GroupService} from '../shared/group.service';
-import {CompanyService} from "../shared/company.service";
-import {Company} from "../shared/company.model";
+import {CompanyService} from '../shared/company.service';
+import {Company} from '../shared/company.model';
+import {ProjectService} from '../shared/project.service';
+import {Project} from '../shared/project.model';
 
 @Component({
   selector: 'app-edit-contract',
@@ -23,15 +25,18 @@ export class EditContractComponent implements OnInit {
   students: Student[] = [];
   group: Group;
   company: Company;
+  project: Project;
 
-  groupContactEmail = 'this is temperary';
+  groupContactEmail = 'this is temperary'; // TODO RKL: Remove.
 
   constructor(private contractSerivce: ContractService, private route: ActivatedRoute, private studentService: StudentService,
-              private groupService: GroupService, private companyService: CompanyService) {
+              private groupService: GroupService, private companyService: CompanyService, private projectService: ProjectService) {
     this.isEditable = false;
     // Defining the properties of the group to avoid undefined property exception.
     this.group = {contactEmail: '', students: []};
     this.company = {name: '', contactName: '', contactPhone: '', contactEmail: ''};
+    // Instantiating the project so we don't get undefined properties. Might be this is doable for company too?
+    this.project = {};
     // Grabbing the url.
     route.params.subscribe(params => {
       // Getting the hashValue from the url. 'contractId' is defined in contract.routing.
@@ -46,6 +51,7 @@ export class EditContractComponent implements OnInit {
       this.populateStudents();
       this.populateGroup();
       this.populateCompany();
+      this.populateProject();
     });
   }
 
@@ -73,6 +79,19 @@ export class EditContractComponent implements OnInit {
       this.companyService.get(this.contract.companyId).subscribe(c => this.company = c);
     }else {
       console.log('No company id');
+    }
+  }
+
+  private populateProject() {
+    if (this.contract.projectId != null) {
+      this.projectService.get(this.contract.projectId).subscribe(p => {
+        this.project = p;
+        console.log('Project Id: ' + this.project.id + ' Project Title: ' + this.project.title);
+      });
+
+    }else {
+      // TODO: Remove when implementation is done.
+      console.log('No project id');
     }
   }
 }
