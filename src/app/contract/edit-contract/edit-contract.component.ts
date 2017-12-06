6dd6520a41193bcd4d91d53267f7321bb0e2f01d
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-12-06 10:45:40 +0100

    Admin can now assign a superVisor

--- src/app/contract/edit-contract/edit-contract.component.ts
+++ src/app/contract/edit-contract/edit-contract.component.ts
@@ -142,4 +142,9 @@ export class EditContractComponent implements OnInit {
       }
     }
   }
+
+  updateAssignedSupervisorOnProject(supervisorId: number) {
+    this.project.assignedSupervisorId = supervisorId;
+    this.projectService.update(this.project).subscribe(() => console.log('Updated supervisor from admin'));
+  }
 }
