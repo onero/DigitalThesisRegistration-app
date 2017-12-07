598df1d9f65b8b844601fe254552d62cf6597084
Adam Hansen <adamlars90@gmail.com>
2017-12-07 11:27:29 +0100

    Implemented appendix

---
+++ src/app/contract/appendix/appendix.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Appendix} from '../shared/appendix.model';
+import {AppendixService} from '../../shared/appendix.service';
+
+@Component({
+  selector: 'app-appendix',
+  templateUrl: './appendix.component.html',
+  styleUrls: ['./appendix.component.css']
+})
+export class AppendixComponent implements OnInit {
+  public conditionCollapsed = true;
+  public resourcesCollapsed = true;
+  appendix: Appendix;
+  constructor(private appendixService: AppendixService) { }
+
+  ngOnInit() {
+    this.appendix = {resources: '', condition: ''};
+    this.appendixService.get().subscribe(a => this.appendix = a);
+  }
+
+}
