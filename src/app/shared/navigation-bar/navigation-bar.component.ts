1321c10216abf8723add425ee996ef6c70a14a93
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-12-08 13:24:26 +0100

    Moved logout button from home to navBar

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    localStorage.setItem('LoggedIn', 'false');
+    localStorage.setItem('Role', '');
+    localStorage.setItem('Token', '');
+    this.router.navigateByUrl('login');
+  }
 }
