64bd2b43019e2056ecace4c53aecf7f71b9c8b54
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-12-11 14:01:41 +0100

    Commiting so Skovgaard can use

--- src/app/contract/new-group/new-group.component.ts
+++ src/app/contract/new-group/new-group.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {GroupService} from '../shared/group.service';
 import {Router} from '@angular/router';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-group',
@@ -9,7 +10,18 @@ import {Router} from '@angular/router';
 })
 export class NewGroupComponent implements OnInit {
 
-  constructor(private router: Router, private groupService: GroupService) { }
+  password: string;
+  confirmPassword: string;
+  passwordGroup: FormGroup;
+  // showPasswordMisMatch = false;
+
+  constructor(private router: Router, private groupService: GroupService, private fb: FormBuilder) {
+    this.passwordGroup = this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(4)]]
+    // });
+    }, {validator: this.confirmPasswordMatchPassword});
+  }
 
   ngOnInit() {
   }
@@ -17,6 +29,32 @@ export class NewGroupComponent implements OnInit {
   create(contactEmail: string) {
     this.groupService.create(contactEmail).subscribe();
     this.router.navigateByUrl('login');
+  }
+
+  confirmPasswordMatchPassword(group: AbstractControl): boolean {
+    const password = group.get('password').value;
+    const confirmPassword = group.get('confirmPassword').value;
+    if (password === confirmPassword) {
+      console.log('Password Match!');
+      // this.showPasswordMisMatch = false;
+      return true;
+    }
+    console.log('Nope');
+    // this.showPasswordMisMatch = true;
+    return false;
+    // return password === confirmPassword;
+  }
+
+  isValid(controlName: string) {
+    const control = this.passwordGroup.controls[controlName];
+
+  }
+
+  isPasswordValid(field: string) {
+    console.log('Hello');
+    const temp = this.passwordGroup.get(field).valid;
+    console.log(temp);
+    return false;
   }
 
 }
