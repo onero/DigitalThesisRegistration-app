d0e15f0ef541798139cfd1b11e49017a042452ba
Sk0vgaard <Sk0vgaard@live.dk>
2017-12-13 12:30:06 +0100

    Refactor

--- src/app/login/new-group/validate.directive.ts
+++ src/app/login/new-group/validate.directive.ts
@@ -15,9 +15,9 @@ export class ValidateDirective implements Validator {
   }
 
   validate(AC: AbstractControl): { [key: string]: any } {
-      // self value (e.g. retype password) - Gets the currentPassword from input.
+      // Gets the currentPassword from input.
       const confirmPassword = AC.value;
-      // control value (e.g. password) - Gets the password from validateEqual in confirmPassword input.
+      // Gets the password from validateEqual in confirmPassword.
       const passwordInputControl = AC.root.get(this.validateEqual);
       // Checks first if there is something in passwordInputControl.
       // If so check if the Password.value if not equal, return something means something is wrong.
