9db6d5b9eab0ca4edac168bd3d2c2bace4b50911
Sk0vgaard <Sk0vgaard@live.dk>
2017-12-13 12:18:09 +0100

    Documentation

--- src/app/login/new-group/validate.directive.ts
+++ src/app/login/new-group/validate.directive.ts
@@ -11,7 +11,8 @@ import { NG_VALIDATORS, Validator, Validators, AbstractControl, ValidatorFn } fr
   ]
 })
 export class ValidateDirective implements Validator {
-  constructor( @Attribute('validateEqual') public validateEqual: string) {}
+  constructor(@Attribute('validateEqual') public validateEqual: string) {
+  }
 
   validate(AC: AbstractControl): { [key: string]: any } {
     // self value (e.g. retype password)
@@ -22,10 +23,20 @@ export class ValidateDirective implements Validator {
 
     // value not equal
     if (equal && value !== equal.value) {
-      return {
-        validateEqual: false
-      };
+      // self value (e.g. retype password) - Gets the currentPassword from input.
+      const confirmPassword = AC.value;
+      // control value (e.g. password) - Gets the password from validateEqual in confirmPassword input.
+      const passwordInputControl = AC.root.get(this.validateEqual);
+      // Checks first if there is something in passwordInputControl.
+      // If so check if the Password.value if not equal, return something means something is wrong.
+      if (passwordInputControl && confirmPassword !== passwordInputControl.value) {
+        return {
+          // needs some kind of return.
+          validateEqual: false
+        };
+      }
+      // The password and conformPassword is the same.
+      return null;
     }
-    return null;
   }
 }
